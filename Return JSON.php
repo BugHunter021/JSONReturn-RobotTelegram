b6c546518256c35f31ab74893cb7cb47f49d7a76
BugHunter021 <76444458+BugHunter021@users.noreply.github.com>
2022-01-11 17:44:03 +0330

    Robot source code

---
+++ Return JSON.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(1);
+$string = json_decode(file_get_contents('php://input'));
+    
+    function objectToArray( $object )
+    {
+        if( !is_object( $object ) && !is_array( $object ) )
+        {
+            return $object;
+        }
+        if( is_object( $object ) )
+        {
+            $object = get_object_vars( $object );
+        }
+        return array_map( 'objectToArray', $object );
+    }
+	$token = '1684441302:AAEZGhiX6blcbhozl-fDx9vyIOGRkmJhdmY';
+	$result = objectToArray($string);
+	$user_id = $result['message']['from']['id'];
+
+// JSON Return   ---------------------------------------
+$json_string = json_encode($result, JSON_PRETTY_PRINT);
+
+	$url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$user_id;
+	$url .= '&text='.urlencode($json_string);
+	$RES =file_get_contents($url);
+	
+ ?>
